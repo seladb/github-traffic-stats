f19eda70926c31c8f2ed80da7517ca72bf8185a7
seladb <seladb@yahoo.com>
2018-02-28 03:47:40 -0500

    Small fix in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'github_traffic_stats2',
   packages = ['github_traffic_stats2'],
-  version = '18.03',
+  version = '18.03.1',
   description = 'A project to pull and store traffic stats for GitHub projects using GitHub API',
   author = 'seladb',
   author_email = 'pcapplusplus@gmail.com',
   entry_points={
-    "console_scripts": ['github_traffic_stats2 = github_traffic_stats2.github_traffic_stats:main']
+    "console_scripts": ['github_traffic_stats = github_traffic_stats2.github_traffic_stats:main']
   },
   url = 'https://github.com/seladb/github-traffic-stats',
   download_url = 'https://github.com/seladb/github-traffic-stats/archive/master.tar.gz',
