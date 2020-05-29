7749fd8b36f7807a14f9ce134a33f14a44670ffa
seladb <pcapplusplus@gmail.com>
2020-05-29 02:02:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+
+setuptools.setup(
   name='github_traffic_stats2',
-  packages=['github_traffic_stats2'],
   version='18.03.1',
   description='A project to pull and store traffic stats for GitHub projects using GitHub API',
   author='seladb',
@@ -11,7 +12,13 @@ setup(
   },
   url='https://github.com/seladb/github-traffic-stats',
   download_url='https://github.com/seladb/github-traffic-stats/archive/master.tar.gz',
+	packages=setuptools.find_packages(),
   keywords=['github', 'github-traffic', 'github-api'],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
   install_requires=['githubpy', 'pickledb', 'simplejson'],
-  classifiers=[],
 )
